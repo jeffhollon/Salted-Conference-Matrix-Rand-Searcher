365dad7646b4d96ebc3e6aef8e94a09884d58cc0
Jeff Hollon <jeffhollon@gmail.com>
2016-05-06 04:27:11 -0400

    comments & cleaned
    Just updated a few lines. No major changes.

--- SCMFinder.m
+++ SCMFinder.m
@@ -1,5 +1,7 @@
 function [ SEQs ] = SCMFinder( N )
 %SCMFINDER Finds 'salted conference matrices' of size N
+%An SCM is a ternary {-1,0,1} object such that its correlation (defined as
+%WW^t has only {-2,0,2} entries in it.  It has exactly one 0 entry per row.
 
     rng shuffle  %set random seed
     
@@ -7,27 +9,28 @@ function [ SEQs ] = SCMFinder( N )
 
     %get N-2 random 1s and -1s
 
-        trial=0;
-
+        trial=0;      
         found=0;
+        
         while ~found
             trial = trial + 1;
             if trial==1000000
                 rng shuffle  %shuffle seed every 1000000 times
                 trial=0;
             end
-            Numbers = round(rand(1,N-2));
-            Numbers( find(Numbers==0) ) = -1;
-            Wtemp=[0, 1, Numbers];
+            
+            Numbers = round(rand(1,N-2)); %get random values for the last N-2 digits
+            Numbers( find(Numbers==0) ) = -1; %make all 0s a -1
+            Wtemp=[0, 1, Numbers];  %assume [0 1 ....] to start
 
-                W=Wtemp(coeffs);
-                Prod=W(1,:)*W';
-                Prod = Prod(1,2:N);
+            W=Wtemp(coeffs);  %makes the big matrix
+            Prod=W(1,:)*W';  %computes the first row of correlation
 
-                if max(abs(Prod)) <= 2
-                    disp(Wtemp)
-                    found=1;
-                end
+            if max(abs(Prod(2:end))) <= 2  %if off diagonal entries are [+-2]
+                %found one!
+                disp(Wtemp)
+                found=1;
+            end
         end
 
 end
