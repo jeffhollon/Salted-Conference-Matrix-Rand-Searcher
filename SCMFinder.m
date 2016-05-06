4845c7fb7d78a4295f57cbe79950f667caaf9668
Jeff Hollon <jeffhollon@gmail.com>
2016-05-06 04:55:04 -0400

    Correctly does non-cyclic
    Handles non-cyclic abelian groups well now.

--- SCMFinder.m
+++ SCMFinder.m
@@ -7,32 +7,34 @@ function [ SEQs ] = SCMFinder( N )
     
     coeffs=getDevMat(N); %get the developed matrix
 
-    %get N-2 random 1s and -1s
+    %in case N is not already just a number, turn it into the number of
+    %elements it should correspond to:
+    N = prod(N);
+    
+    trial=0;      
+    found=0;
 
-        trial=0;      
-        found=0;
-        
-        while ~found
-            trial = trial + 1;
-            if trial==1000000
-                rng shuffle  %shuffle seed every 1000000 times
-                trial=0;
-            end
-            
-            Numbers = round(rand(1,N-2)); %get random values for the last N-2 digits
-            Numbers( find(Numbers==0) ) = -1; %make all 0s a -1
-            Wtemp=[0, 1, Numbers];  %assume [0 1 ....] to start
-
-            W=Wtemp(coeffs);  %makes the big matrix
-            Prod=W(1,:)*W';  %computes the first row of correlation
-
-            if max(abs(Prod(2:end))) <= 2  %if off diagonal entries are [+-2]
-                %found one!
-                disp(Wtemp)  %show the sequence
-                disp(Prod)  %Show the correlation
-                found=1;
-            end
+    while ~found
+        trial = trial + 1;
+        if trial==1000000
+            rng shuffle  %shuffle seed every 1000000 times
+            trial=0;
         end
+
+        Numbers = round(rand(1,N-2)); %get random values for the last N-2 digits
+        Numbers( find(Numbers==0) ) = -1; %make all 0s a -1
+        Wtemp=[0, 1, Numbers];  %assume [0 1 ....] to start
+
+        W=Wtemp(coeffs);  %makes the big matrix
+        Prod=W(1,:)*W';  %computes the first row of correlation
+
+        if max(abs(Prod(2:end))) <= 2  %if off diagonal entries are [+-2]
+            %found one!
+            disp(Wtemp)  %show the sequence
+            disp(Prod)  %Show the correlation
+            found=1;
+        end
+    end
 
 end
 
